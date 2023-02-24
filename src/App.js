38574fc786b4d74ed93553374c8246404e8686aa
FrancescaBambozzi <bambozzif@gmail.com>
2023-02-24 21:08:42 +0100

    fetched data from API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Podcasts coming soon ...
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      podcasts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          podcasts: json.feed.entry
+        });
+      })
+  }
+
+  render() {
+    const { podcasts } = this.state;
+    return (
+      <div className="App">
+        <h1>Podcaster</h1>
+        <div className="content">
+          { podcasts.map((item) => (
+            <ol key= {item.id.attributes["im:id"]}>
+              Title: {item.title.label}
+            </ol>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
